fcf89adccb60125bd8fb5a9e092ac30395c64138
The Vampiro Elegant <streetdemonic003@gmail.com>
2023-07-21 19:10:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "6159007907:AAGOYPXHxyqfJ3GflNn0zIDNJXkNhpOcSL8"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
